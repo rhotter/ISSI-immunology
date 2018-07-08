7d8b99def7e50e379ce3f83aeab337402094f6fc
Raphael Hotter <rhotter@users.noreply.github.com>
2018-07-08 10:30:24 +0300

    j

--- remove-dups.py
+++ remove-dups.py
@@ -4,14 +4,14 @@ import csv
 
 with open("../data/smMIP_Old_P7_index17_S17.sorted.rg.realigned.freq.paired.Q30.txt","r") as f:
     readerList = list(csv.reader(f, delimiter='\t'))
+    headings = readerList[0]
 
-readerList = sorted(readerList, key = lambda x: x[1])
+readerList = sorted(readerList[1:], key = lambda x: x[1])
 
 previous = []
 i = 0
 while i in range(0,len(readerList)):
     if readerList[i][1] == previous:
-        print('dup')
         readerList.pop(i)
     previous = readerList[i][1]
     i += 1
@@ -19,5 +19,6 @@ while i in range(0,len(readerList)):
 
 with open("../data/newFrequency.txt","w") as f:
     writer = csv.writer(f, delimiter = "\t")
+    writer.writerow(headings)
     for row in readerList:
         writer.writerow(row)
