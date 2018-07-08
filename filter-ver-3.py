bf15c36db6893661a4dc42f10bb96a166e018023
Raphael Hotter <rhotter@users.noreply.github.com>
2018-07-08 15:14:21 +0300

    j

--- filter-ver-3.py
+++ filter-ver-3.py
@@ -9,8 +9,11 @@ import csv
 from scipy import stats
 
 # list of inclusion and exclusion filters by column and value
-incFilters = [(1,["chr10"]),(4,["T"])]
-excFilters = [(6,["UTR5"])]
+# incFilters = [(1,["chr10"]),(4,["T"])]
+# excFilters = [(6,["UTR5"])]
+incFilters=[]
+excFilters=[]
+
 
 # itterates through filters and return true if value corresponds in column
 def inclusionItterator(x):
@@ -25,8 +28,11 @@ def exclusionItterator(x):
             return False
     return True
 
-def chi_squared(n1,n2,N1,N2):
-    return stats.fisher_exact([[n1,N1-n1],[n2,N2-n2]])[1]
+def chi_squaredFunc(n1,n2,N1,N2,c):
+    if n1 < 0 or N1-n1 < 0 or n2 < 0 or N2-n2 < 0:
+        return -1
+    else:
+        return stats.fisher_exact([[n1,N1-n1],[n2,N2-n2]])[1]
 
 # Part 1: Takes tab separated txt file as input and stores filtered rows in array
 with open("../data/total.txt","r") as f:
@@ -55,11 +61,25 @@ with open("../data/frequencyFiles/smMIP_Old_P7_index17_S17.sorted.rg.realigned.f
     next(f)
     freqList = list(csv.reader(f, delimiter='\t'))
 filteredTotal = list(filter(lambda x: x[0]=='smMIP_Old_P7_index17_S17', fullFiltered))
+filteredTotal = sorted(filteredTotal, key = lambda x: x[2])
 filteredTotalPositions = [row[2] for row in filteredTotal]
 filteredFreq = list(filter(lambda x: x[1] in filteredTotalPositions, freqList))
+filteredFreq = sorted(filteredFreq, key = lambda x: x[1])
 
-print(len(filteredTotalPositions))
-print(len(filteredFreq))
+# with open("../data/freqList.txt","w") as f:
+#     writer = csv.writer(f, delimiter ="\t")
+#     for row in freqList:
+#         writer.writerow(row)
+#
+# with open("../data/filteredTotalPositions.txt","w") as f:
+#     writer = csv.writer(f, delimiter ="\t")
+#     for row in filteredTotalPositions:
+#         writer.writerow(row)
+#
+# with open("../data/filteredFreq.txt","w") as f:
+#     writer = csv.writer(f, delimiter ="\t")
+#     for row in filteredFreq:
+#         writer.writerow(row)
 
 mutations = {
     'A': [6,7],
@@ -75,11 +95,17 @@ i = 0
 # fix for duplicate totals
 while i in range(0,len(filteredTotal)):
     j = i - k
-    if filteredTotal[i][2] == filteredFreq[j]:
-        mut = mutations[filteredTotal[j][6]]
-        chi_squared = chi_squared(filteredFreq[j][mut[0]],filteredFreq[j][mut[1]],filteredFreq[j][4],filteredFreq[j][5])
-        newRow = filteredTotal[i]
-        filteredTotalChi.append(newRow.append(chi_squared))
+    if j < 0:
+        print(i,j)
+        print(filteredTotal[i][2])
+    if filteredTotal[i][2] == filteredFreq[j][1]:
+        mut = mutations[filteredTotal[j][5]]
+        chi_squared = chi_squaredFunc(int(filteredFreq[j][mut[0]]),
+                                      int(filteredFreq[j][mut[1]]),
+                                      int(filteredFreq[j][4]),
+                                      int(filteredFreq[j][5]),
+                                      filteredTotal[i][2])
+        filteredTotalChi.append(filteredTotal[i]+[str(chi_squared)])
         i += 1
     else:
         k += 1
@@ -92,6 +118,6 @@ with open("../data/filteredTotal.txt", "w") as f:
 
 with open("../data/filteredTotalChi.txt","w") as f:
     writer = csv.writer(f, delimiter ="\t")
-    # writer.writerow(headings.append("p"))
+    writer.writerow(headings +['p'])
     for row in filteredTotalChi:
         writer.writerow(row)
