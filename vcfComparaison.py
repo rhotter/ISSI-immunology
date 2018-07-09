98a3b6f5a66cb70e198202d0bb663cecfe56b4e2
yoav nir <11yoav.nir@gmail.com>
2018-07-09 13:40:51 +0300

    gh

--- vcfComparaison.py
+++ vcfComparaison.py
@@ -1,4 +1,31 @@
 import csv
+patient = "smMIP_Old_P7_index17_S17"
 
-with open("../data/vcf/" + patient + ".sorted.rg.realigned." + vcfType + ".annovar.vcf.hg19_multianno.vcf", "r") as f:
+firstSample = []
+secondSample = []
+commonSample = []
+
+with open("../data/vcf/filtered/" + patient + ".sorted.rg.realigned.platypus.annovar.vcf.hg19_multianno-filtered.txt", "r") as f:
     readerList = list(csv.reader(f, delimiter='\t'))
+    headings = readerList[0]
+    for row in readerList[1:]:
+        firstSample.append(row)
+
+with open("../data/vcf/filtered/" + patient + ".sorted.rg.realigned.varscan.annovar.vcf.hg19_multianno-filtered.txt","r") as f:
+    next(f)
+    readerList = list(csv.reader(f, delimiter='\t'))
+    for row in readerList:
+        secondSample.append(row)
+
+for r in firstSample:
+    for s in secondSample:
+        if r[0:2] == s[0:2]:
+            commonSample.append(s)
+
+with open("../data/vcf/filtered/commons/" + patient + ".sorted.rg.realigned.compared.annovar.vcf.hg19_multianno-filtered.txt","w") as f:
+    writer = csv.writer(f, delimiter="\t")
+    writer.writerow(headings)
+    for row in commonSample:
+        writer.writerow(row)
+
+print(commonSample)
